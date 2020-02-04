e409d0dc9d360173b57a801205b10270f74cd9c0
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-02-04 16:11:24 +0900

    html mock

---
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam04")
+public class Exam04Controller {
+
+}
